41cc4a9d67f743de51411c6b90e465abbc887609
HemangPatel1 <hemang2@gmail.com>
2013-11-12 17:26:07 +0000

    made score tally dynamic

--- lib/rps.rb
+++ lib/rps.rb
@@ -13,25 +13,25 @@ DataMapper.auto_upgrade!
 class RPS < Sinatra::Base
 set :views, File.join(File.dirname(__FILE__), '..', 'views')
 
-  get '/' do
-		@score = Score.last.winner
-		erb :index
-  end
+	get '/' do
+	  @score = Score.last.winner
+	  erb :index
+	end
 
 	post '/score' do
-		@winner = params[:winner]
-    Score.create(:winner => @winner) 
-		puts "#{@winner}"
+	  @winner = params[:winner]
+	  Score.create(:winner => @winner) 
+	  puts "#{@winner}"
 	end
 
 	get '/score_computer' do
-		Score.count(:winner.like => "Computer").to_s
+	  Score.count(:winner.like => "Computer").to_s
 	end
 
 	get '/score_player' do
-		Score.count(:winner.like => "You").to_s
+	  Score.count(:winner.like => "You").to_s
 	end
 
 	# start the server if ruby file executed directly
-  run! if app_file == $0
+	run! if app_file == $0
 end
